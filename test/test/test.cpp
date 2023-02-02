2bf43d0647966600bdf425da91e7a55248ab5d19
Sesha <ArPaSokolov@gmail.com>
2023-02-02 17:17:56 +0200

    фиксим 2

---
+++ test/test/test.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    short c = 12;
+    int d = 10;
+    short s = 2;
+
+    short* pc = &c;          // получаем адрес переменной с типа short
+    int* pd = &d;           // получаем адрес переменной d типа int
+    short* ps = &s;         // получаем адрес переменной s типа short
+
+    std::cout << "Variable c: address=" << pc << "\t value=" << *pc << std::endl;
+    std::cout << "Variable d: address=" << pd << "\t value=" << *pd << std::endl;
+    std::cout << "Variable s: address=" << ps << "\t value=" << *ps << std::endl;
+}
