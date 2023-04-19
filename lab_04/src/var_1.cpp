e228a5f04dbb2d84e0070cad90b6589a0563ee5c
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-04-19 23:30:34 +0200

    lab 4 pased

--- lab_04/src/var_1.cpp
+++ lab_04/src/var_1.cpp
@@ -32,26 +32,28 @@ public:
 		}
 	}
 
+	MyVectorBool(MyVectorBool& V) {
+		size = V.size;
+		binaryN = (bool*)calloc(size, sizeof(bool)); // выделяем память под бинарную запись числа
+		if (binaryN != NULL) { // проверяем, выделилась ли память успешно
+			for (int i = 0; i < size; i++) {
+				binaryN[i] = V.binaryN[i]; // заполняем новый массив
+			}
+		}
+		else { // память не выделилась
+			cout << "Error 0: could not allocate memory" << endl;
+		}
+	}
+
 	MyVectorBool() { // конструктор по умолчанию
 		size = 0;
-		binaryN = (bool*)calloc(size, sizeof(bool));
+		binaryN = nullptr;
 	}
 
 	friend ostream& operator << (ostream& out, const MyVectorBool& V) { // оператор вывода
 		cout << "Binary: ";
-		bool flag = false; // флаг на встречу с единицей
-		int i = 0; // счетчик
-		while (i < V.size) { // цикл для вывода бинарной записи
-			if (flag == false && V.binaryN[i] == 1) { // если данный элемент равен единице и единица не встречалась до этого
-				flag = true; // флаг - встретили единицу
-			}
-			if (flag == true) { // если встретили единицу 
-				out << V.binaryN[i];  // выводим элемент
-			}
-			i++;
-		}
-		if (flag == false) { // если единица так и не встретилась
-			cout << 0; // выводим один нолик
+		for (int i = 0; i < V.size; i++) {
+			out << V.binaryN[i];
 		}
 		cout << endl;
 		return out;
@@ -71,70 +73,37 @@ public:
 		}
 	}
 
-	friend MyVectorBool& operator + (MyVectorBool& V1, MyVectorBool& V2) { // оператор сложения
-
-		int maxSize = max(V1.size, V2.size); // находим самое длинное число
+	friend MyVectorBool operator + (MyVectorBool& V1, MyVectorBool& V2)
+	{
+		int maxSize = max(V1.size, V2.size);
 		if (V1.size < maxSize) { // выравниваем количество элементов
-			bool* newBinaryN = (bool*)calloc(V1.size, sizeof(bool)); // создаем новый массив для временного хранения элементов
-			for (int i = 0; i < maxSize; i--) {
-				newBinaryN[i] = V1.binaryN[i]; // заполняем новый массив
-			}
-			V1.binaryN = (bool*)realloc(V1.binaryN, maxSize * sizeof(bool)); // выделяем память под новый элемент
-			if (V1.binaryN != NULL) { // проверяем, выделилась ли память успешно
-				for (int i = V1.size; i > 0; i--) {
-					V1.binaryN[i] = newBinaryN[i];
-					cout << V1.binaryN[i];
-				}
-				for (int i = V1.size; i < maxSize; i++) {
-					V1.binaryN[i] = 0;
-					cout << V1.binaryN[i];
-				}
-				cout << endl;
-				free(newBinaryN); // чистим память
-			}
-			else { // память не выделилась
-				cout << "Error 01: could not allocate memory" << endl;
+			for (int i = V1.size; i < maxSize; i++) {
+				V1.add_start(0);
 			}
 		}
-		if (V2.size < maxSize) { // выравниваем количество элементов
-			bool* newBinaryN = (bool*)calloc(V2.size, sizeof(bool)); // создаем новый массив для временного хранения элементов
-			for (int i = 0; i < maxSize; i++) {
-				newBinaryN[i] = V2.binaryN[i]; // заполняем новый массив
-			}
-			V2.binaryN = (bool*)realloc(V2.binaryN, maxSize * sizeof(bool)); // выделяем память под новый элемент
-			if (V2.binaryN != NULL) { // проверяем, выделилась ли память успешно
-				for (int i = V2.size; i > 0; i--) {
-					V2.binaryN[i] = newBinaryN[i - 1];
-					cout << V2.binaryN[i];
-				}
-				for (int i = V2.size; i < maxSize; i++) {
-					V2.binaryN[i] = 0;
-					cout << V2.binaryN[i];
-				}
-				cout << endl;
-				free(newBinaryN); // чистим память
-			}
-			else { // память не выделилась
-				cout << "Error 02: could not allocate memory" << endl;
+		else { // выравниваем количество элементов
+			for (int i = V2.size; i < maxSize; i++) {
+				V2.add_start(0);
 			}
 		}
 		int carry = 0; // инициализируем перенос
 		MyVectorBool temp; // создаем новый массив для хранения суммы
-		temp.binaryN = (bool*)calloc(maxSize, sizeof(bool)); // выделяем память
-		for (int i = maxSize; i > 0; i--) { // бежим по парам элементов
+		temp.size = maxSize;
+		temp.binaryN = (bool*)calloc(temp.size, sizeof(bool)); // выделяем память
+		for (int i = (maxSize - 1); i >= 0; i--) { // бежим по парам элементов
 			if (V1.binaryN[i] == 0 && V2.binaryN[i] == 0 && carry == 0) {
 				temp.binaryN[i] = 0;
 				carry = 0;
 			}
-			else if (V1.binaryN[i] == 1 && V2.binaryN[i] == 0 && carry == 0) {
+			else if ((V1.binaryN[i] == 1 && V2.binaryN[i] == 0 && carry == 0) || 
+				(V1.binaryN[i] == 0 && V2.binaryN[i] == 1 && carry == 0) ||
+				(V1.binaryN[i] == 0 && V2.binaryN[i] == 0 && carry == 1)) {
 				temp.binaryN[i] = 1;
 				carry = 0;
 			}
-			else if (V1.binaryN[i] == 0 && V2.binaryN[i] == 1 && carry == 0) {
-				temp.binaryN[i] = 1;
-				carry = 0;
-			}
-			else if (V1.binaryN[i] == 1 && V2.binaryN[i] == 1 && carry == 0) {
+			else if ((V1.binaryN[i] == 1 && V2.binaryN[i] == 1 && carry == 0) ||
+				(V1.binaryN[i] == 1 && V2.binaryN[i] == 0 && carry == 1) ||
+				(V1.binaryN[i] == 0 && V2.binaryN[i] == 1 && carry == 1)) {
 				temp.binaryN[i] = 0;
 				carry = 1;
 			}
@@ -142,35 +111,16 @@ public:
 				temp.binaryN[i] = 1;
 				carry = 1;
 			}
-			else if (V1.binaryN[i] == 1 && V2.binaryN[i] == 0 && carry == 1) {
-				temp.binaryN[i] = 0;
-				carry = 1;
-			}
-			else if (V1.binaryN[i] == 0 && V2.binaryN[i] == 1 && carry == 1) {
-				temp.binaryN[i] = 0;
-				carry = 1;
-			}
-			else if (V1.binaryN[i] == 0 && V2.binaryN[i] == 0 && carry == 1) {
-				temp.binaryN[i] = 1;
-				carry = 0;
-			}
-			//if (carry == 1 && (i + 1) == maxSize) { // единичка осталась, а длина закончилась
-			//	temp.size++;
-			//	temp.binaryN = (bool*)realloc(temp.binaryN, (maxSize + 1)* sizeof(bool)); // удлиняем на один элемент
-			//	temp.binaryN[maxSize] = 1;
-			//}
-			/*cout << temp.binaryN[i] << endl;*/
+		}
+		if (carry == 1) {
+			temp.add_start(1);
 		}
 		return temp;
 	}
 
-	bool operator[](int index) const { // оператор получения элемента по индексу
+	bool& operator[](int index) { // оператор изменения элемента по индексу
 		return binaryN[index];
 	}
-
-	/*bool& operator[](int index) {
-		return binaryN[index];
-	}*/
 
 	void add_start(bool k) { // добавление элемента в начало
 		bool* newBinaryN = (bool*)calloc(size, sizeof(bool)); // создаем новый массив для временного хранения элементов
@@ -246,6 +196,36 @@ public:
 	~MyVectorBool() { // деструктор
 		free(binaryN);
 	}
+
+	//----------------------------------------------------------- доп задание ------------------------------------------------
+	friend MyVectorBool operator / (MyVectorBool& V1, MyVectorBool& V2) { // перемешать элементы двух массивов начиная с конца
+		int sumSize = V1.size + V2.size; // находим общую длину
+		MyVectorBool temp; // новый вектор для хранения
+		temp.size = sumSize;
+		temp.binaryN = (bool*)calloc(temp.size, sizeof(bool)); // выделяем память
+		if (V1.size > V2.size) { // случай когда первое число больше чем второе
+			int rest = V1.size - V2.size; // разница в длине
+			for (int i = 0; i < (rest - 1) || i < (V1.size - 1); i++) {
+				temp.binaryN[i] = V1.binaryN[i];
+			}
+			int j = rest;
+			int i = 0;
+			int m = rest;
+			while (m <= (temp.size - 1) && j <= (V1.size - 1)) { // закидываем элементы из первого через 1
+				temp.binaryN[m] = V1.binaryN[j];
+				m += 2;
+				j++;
+			}
+			m = rest + 1;
+			while (m <= (temp.size - 1) && i <= (V2.size - 1)) { // закидываем элементы из второго через 1
+				temp.binaryN[m] = V2.binaryN[i];
+				m += 2;
+				i++;
+			}
+		}
+		return temp;
+	}
+	//----------------------------------------------------------- доп задание ------------------------------------------------
 };
 
 
@@ -254,35 +234,42 @@ int main() {
 	cout << "Enter a decimal number A: ";
 	cin >> N; // вводим десятичное число
 
-	MyVectorBool A(N);
-	/*A.add_start(1);*/
+	MyVectorBool A(N);/*
+	A.add_start(0);*/
 	cout << A;
 
 	int M;
 	cout << "Enter a decimal number B: ";
 	cin >> M; // вводим десятичное число
 
-	MyVectorBool B(M);
-	/*B.add_end(0);*/
+	MyVectorBool B(M);/*
+	B.add_end(0)*/;
 	cout << B;
-
-	//cout << "Enter index from 1 to " << B.GetSize() << " to delete: ";
-	//int k;
-	//cin >> k;
-	//B.erase(k);
-	//cout << B;
-
-	//cout << "Enter index from 1 to " << B.GetSize() << " : ";
-	//int i;
-	//cin >> i;
-	//cout << B[i] << endl;
-	///*cout << "Enter index from 1 to " << B.GetSize() << " to change: "*/;
-	///*B[i] = 1;*/
-	//cout << B;
 
 	MyVectorBool C;
 	C = A + B;
+	cout << "A + B" << endl;
 	cout << C;
+
+	// доп задание
+	C = A / B;
+	cout << C;
+
+	/*cout << "Enter index from 1 to " << B.GetSize() << " to delete: ";
+	int k;
+	cin >> k;
+	B.erase(k);
+	cout << B;
+
+	cout << "Enter index from 1 to " << B.GetSize() << " to change: ";
+	int i;
+	bool k;
+	cin >> i;
+	cout << "Change to: ";
+	cin >> k;
+	B[i-1] = k;
+	cout << B;*/
+
 
 	return 0;
 }
