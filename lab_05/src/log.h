0926f52adbd66d3a5fa43907f724a5241f71b365
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-04-25 18:41:32 +0200

    Логгер вводится из main

--- lab_05/src/log.h
+++ lab_05/src/log.h
@@ -33,50 +33,50 @@ public:
 		m_logLevel = logLevel;
 	}
 
-	static void Info()
+	static void Info(const std::string& msg)
 	{
 		time_t t = time(nullptr);
 		tm ptm = *localtime(&t);
 		if (m_logLevel == Level::INFO) {
-			std::cerr << "\x1b[32m" << "[INFO]["<< std::put_time(&ptm, "%T") << "]Hello World" << "\x1b[0m" << std::endl;
-			m_out << "{I}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]Hello World" << std::endl;
+			std::cerr << "\x1b[32m" << "[INFO]["<< std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
+			m_out << "{I}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 		}
 	}
 
-	static void Debug()
+	static void Debug(const std::string& msg)
 	{
 		time_t t = time(nullptr);
 		tm ptm = *localtime(&t);
 		if (m_logLevel == Level::DEBUG) {
-			std::cerr << "\x1b[38;5;244m" << "[DEBUG][" << std::put_time(&ptm, "%T") << "]Program is running" << "\x1b[0m" << std::endl;
-			m_out << "{D}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]Program is running" << std::endl;
+			std::cerr << "\x1b[38;5;244m" << "[DEBUG][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
+			m_out << "{D}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 		}
 	}
 
-	static void Error()
+	static void Error(const std::string& enmbr, const std::string& msg)
 	{
 		time_t t = time(nullptr);
 		tm ptm = *localtime(&t);
 		if (m_logLevel == Level::ERROR) {
-			std::cerr << "\x1b[31m" << "[ERROR#00][" << std::put_time(&ptm, "%T") << "]Invalid data type" << "\x1b[0m" << std::endl;
-			m_out << "[[ERROR#00][" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]Invalid data type" << std::endl;
+			std::cerr << "\x1b[31m" << "[ERROR#" << enmbr << "][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
+			m_out << "[[ERROR#" << enmbr <<"][" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 		}
 	}
 
-	static void Release()
+	static void Release(const std::string& msg)
 	{
 		time_t t = time(nullptr);
 		tm ptm = *localtime(&t);
 		if (m_logLevel == Level::RELEASE) {
-			std::cerr << "\x1b[38;5;244m" << "[RELEASE][" << std::put_time(&ptm, "%T") << "]Program is running" << "\x1b[0m" << std::endl;
-			m_out << "{R}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]Program is running" << std::endl;
+			std::cerr << "\x1b[38;5;244m" << "[RELEASE][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
+			m_out << "{R}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 		}
 	}
-	static void End()
+	static void End(const std::string& msg)
 	{
 		time_t t = time(nullptr);
 		tm ptm = *localtime(&t);
-		std::cerr << "\x1b[38;5;244m" << "[END][" << std::put_time(&ptm, "%T") << "]Program ended" << "\x1b[0m" << std::endl;
-		m_out << "{0}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]Program ended" << std::endl;
+		std::cerr << "\x1b[38;5;244m" << "[END][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
+		m_out << "{0}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 	}
 };
--- lab_05/src/main.cpp
+++ lab_05/src/main.cpp
@@ -17,21 +17,24 @@ int main()
 
 	if (Dflag == true) {
 		Log::SetLogLevel(static_cast<Log::Level>(0));
-		Log::Debug();
+		Log::Debug("Program is running");
 	}
 	if (Rflag == true) {
 		Log::SetLogLevel(static_cast<Log::Level>(1));
-		Log::Release();
+		Log::Release("Program is running");
 	}
 	if (Iflag == true) {
 		Log::SetLogLevel(static_cast<Log::Level>(2));
-		Log::Info();
+		Log::Info("Hello World");
+		Log::Info("Booyakasha");
 	}
 	if (Eflag == true) {
 		Log::SetLogLevel(static_cast<Log::Level>(3));
-		Log::Error();
+		Log::Error("001","Invalid data type");
+		Log::Error("002","Out of range");
+		Log::Error("003","Smth in the way");
 	}
 
-	Log::End();
+	Log::End("Program finished successfully");
 	return 0;
 }
