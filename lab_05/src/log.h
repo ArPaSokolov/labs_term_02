ae98c11c7f9707ee0ba916799b06082e02fb7bfb
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2023-05-04 17:24:43 +0200

    comments added

--- lab_05/src/log.h
+++ lab_05/src/log.h
@@ -21,75 +21,75 @@ private:
 
 	static std::ofstream m_out;
 	static Level m_logLevel;
-	static bool flag[4];
+	static bool flag[4]; // поле для хранения "галочек"
 
 public:
 	
-	static void SetLogPath(const std::string& path)
+	static void SetLogPath(const std::string& path) // установливаем путь до файла с логами
 	{
 		m_out.open(path, std::ios::app);
 	}
 
-	static void SetLogLevel(Level logLevel)
+	static void SetLogLevel(Level logLevel) // установщик уровня доступа
 	{
 		m_logLevel = logLevel;
 	}
 
-	static void LevelFlag() {
+	static void LevelFlag() { // метод для установки "галочек"
 		std::cout << "DEBUG, RELEASE, INFO, ERROR: ";
 		for (int i = 0; i < 4; i++) {
-			std::cin >> flag[i];
+			std::cin >> flag[i]; // "1" - галочка стоити, "0" - не стоит
 		}
 	}
 
-	static void AutoLog(int logLevel, const std::string& msg) {
-		if (Log::flag[logLevel] == true) {
-			Log::SetLogLevel(static_cast<Log::Level>(logLevel));
-			if (m_logLevel == Level::INFO) {
-				time_t t = time(nullptr);
-				tm ptm = *localtime(&t);
+	static void AutoLog(int logLevel, const std::string& msg) { // автологгер
+		if (Log::flag[logLevel] == true) { // галочка на INFO стоит
+			Log::SetLogLevel(static_cast<Log::Level>(logLevel)); // открываем уровень
+			if (m_logLevel == Level::INFO) {  
+				time_t t = time(nullptr); // время
+				tm ptm = *localtime(&t); // время
 				std::cerr << "\x1b[32m" << "[INFO][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
 				m_out << "{I}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 			}
 		}
-		if (flag[logLevel] == true) {
-			Log::SetLogLevel(static_cast<Log::Level>(logLevel));
+		if (flag[logLevel] == true) { // галочка на DEBUG стоит
+			Log::SetLogLevel(static_cast<Log::Level>(logLevel)); // открываем уровень
 			if (m_logLevel == Level::DEBUG) {
-				time_t t = time(nullptr);
-				tm ptm = *localtime(&t);
+				time_t t = time(nullptr); // время
+				tm ptm = *localtime(&t); // время
 				std::cerr << "\x1b[38;5;244m" << "[DEBUG][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
 				m_out << "{D}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 			}
 		}
-		if (flag[logLevel] == true) {
-			Log::SetLogLevel(static_cast<Log::Level>(logLevel));
+		if (flag[logLevel] == true) { // галочка на RELEASE стоит
+			Log::SetLogLevel(static_cast<Log::Level>(logLevel)); // открываем уровень
 			if (m_logLevel == Level::RELEASE) {
-				time_t t = time(nullptr);
-				tm ptm = *localtime(&t);
+				time_t t = time(nullptr); // время
+				tm ptm = *localtime(&t); // время
 				std::cerr << "\x1b[38;5;244m" << "[RELEASE][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
 				m_out << "{R}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 			}
 		}
 	}
 
-	static void AutoLog(int logLevel, const std::string& msg, const std::string& enmbr) {
-		if (flag[logLevel] == true) {
-			Log::SetLogLevel(static_cast<Log::Level>(logLevel));
+	static void AutoLog(int logLevel, const std::string& msg, const std::string& enmbr) { // ERROR
+		if (flag[logLevel] == true) { // галочка на ERROR стоит
+			Log::SetLogLevel(static_cast<Log::Level>(logLevel)); // открываем уровень
 			if (m_logLevel == Level::ERROR) {
-				time_t t = time(nullptr);
-				tm ptm = *localtime(&t);
+				time_t t = time(nullptr); // время
+				tm ptm = *localtime(&t); // время
 				std::cerr << "\x1b[31m" << "[ERROR#" << enmbr << "][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
 				m_out << "[[ERROR#" << enmbr << "][" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 			}
 		}
 	}
 
-	static void End(int logLevel, const std::string& msg)
+	static void End(int logLevel, const std::string& msg) // программа завершилась успешно
 	{
-		Log::SetLogLevel(static_cast<Log::Level>(logLevel));
+		Log::SetLogLevel(static_cast<Log::Level>(logLevel)); // открываем уровень
 		if (m_logLevel == Level::END) {
-			time_t t = time(nullptr);
-			tm ptm = *localtime(&t);
+			time_t t = time(nullptr); // время
+			tm ptm = *localtime(&t); // время
 			std::cerr << "\x1b[38;5;244m" << "[END][" << std::put_time(&ptm, "%T") << "]" << msg << "\x1b[0m" << std::endl;
 			m_out << "{0}[" << std::put_time(&ptm, "%d:%m:%Y %H:%M:%S") << "]" << msg << std::endl;
 		}
--- lab_05/src/main.cpp
+++ lab_05/src/main.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main()
 {
-	Log::SetLogPath("../log.txt");
+	Log::SetLogPath("../log.txt"); // указываем путь до файла с логами
 
-	Log::LevelFlag();
+	Log::LevelFlag(); // метод для установки "галочек"
 
 	Log::AutoLog(0, "Program is running");
 
 	Log::AutoLog(1, "Program is running");
 
 	Log::AutoLog(2, "Hello World");
-	Log::AutoLog(2,"Booyakasha");
+	Log::AutoLog(2, "Booyakasha");
 
-	Log::AutoLog(3,"Invalid data type", "001");
-	Log::AutoLog(3,"Out of range", "002");
-	Log::AutoLog(3,"Slishkom mnogo bukav", "001");
+	Log::AutoLog(3, "Invalid data type", "001");
+	Log::AutoLog(3, "Out of range", "002");
+	Log::AutoLog(3, "Slishkom mnogo bukav", "001");
 
-	Log::End(4,"Program finished successfully");
+	Log::End(4, "Program finished successfully");
 	return 0;
 }
